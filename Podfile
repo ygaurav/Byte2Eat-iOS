9fe61f2ec46286958acc74a1c982663edffa8b48
Gaurav Yadav <ygaurav@gmail.com>
2014-07-16 01:18:04 +0530

    Removing Pop

--- Podfile
+++ Podfile
@@ -3,4 +3,3 @@ platform :ios, '7.0'
 xcodeproj 'Byte2Eat.xcodeproj'
 
 pod 'RestKit', '~> 0.22.0', :inhibit_warnings => true
-pod "pop"
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -72,9 +72,3 @@
 #define COCOAPODS_VERSION_MINOR_TransitionKit 0
 #define COCOAPODS_VERSION_PATCH_TransitionKit 0
 
-// pop
-#define COCOAPODS_POD_AVAILABLE_pop
-#define COCOAPODS_VERSION_MAJOR_pop 1
-#define COCOAPODS_VERSION_MINOR_pop 0
-#define COCOAPODS_VERSION_PATCH_pop 2
-
